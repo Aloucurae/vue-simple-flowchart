40555be415990700586fcfef509fb4edc701d5e1
Alex.js <alexj.desantanna@gmail.com>
2020-02-04 18:30:58 -0300

    ajuste de dependencias e identação

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,31 @@
 <template>
   <div id="app">
-    <h1> simple flowchart</h1>
+    <h1>simple flowchart</h1>
     <div class="tool-wrapper">
       <select v-model="newNodeType">
         <option v-for="(item, index) in nodeCategory" :key="index" :value="index">{{item}}</option>
       </select>
-      <input type="text" v-model="newNodeLabel" placeholder="Input node label">
+      <input type="text" v-model="newNodeLabel" placeholder="Input node label" />
       <button @click="addNode">ADD</button>
     </div>
-    
-    <simple-flowchart :scene.sync="scene" 
+
+    <simple-flowchart
+      :scene.sync="scene"
       @nodeClick="nodeClick"
       @nodeDelete="nodeDelete"
       @linkBreak="linkBreak"
       @linkAdded="linkAdded"
       @canvasClick="canvasClick"
-      :height="800"/>
+      :height="800"
+    />
   </div>
 </template>
 
 <script>
-import SimpleFlowchart from './components/SimpleFlowchart.vue'
+import SimpleFlowchart from "./components/SimpleFlowchart.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     SimpleFlowchart
   },
@@ -38,79 +40,80 @@ export default {
             id: 2,
             x: -700,
             y: -69,
-            type: 'Action',
-            label: 'test1',
+            type: "Action",
+            label: "test1"
           },
           {
             id: 4,
             x: -357,
             y: 80,
-            type: 'Script',
-            label: 'test2',
+            type: "Script",
+            label: "test2"
           },
           {
-            id: 6,
+            id: 60,
             x: -557,
             y: 80,
-            type: 'Rule',
-            label: 'test3',
+            type: "Rule",
+            label: "test3"
           }
         ],
         links: [
           {
             id: 3,
             from: 2, // node id the link start
-            to: 4,  // node id the link end
+            to: 4 // node id the link end
+          },
+          {
+            id: 30,
+            from: 2, // node id the link start
+            to: 60 // node id the link end
           }
         ]
       },
       newNodeType: 0,
-      newNodeLabel: '',
-      nodeCategory:[
-        'rule',
-        'action',
-        'script',
-        'decision',
-        'fork',
-        'join',
-      ],
-    }
+      newNodeLabel: "",
+      nodeCategory: ["rule", "action", "script", "decision", "fork", "join"]
+    };
   },
   methods: {
     canvasClick(e) {
-      console.log('canvas Click, event:', e)
+      window.console.log("canvas Click, event:", e);
     },
     addNode() {
-      let maxID = Math.max(0, ...this.scene.nodes.map((link) => {
-        return link.id
-      }))
+      let maxID = Math.max(
+        0,
+        ...this.scene.nodes.map(link => {
+          return link.id;
+        })
+      );
       this.scene.nodes.push({
         id: maxID + 1,
         x: -400,
         y: 50,
         type: this.nodeCategory[this.newNodeType],
-        label: this.newNodeLabel ? this.newNodeLabel: `test${maxID + 1}`,
-      })
+        label: this.newNodeLabel ? this.newNodeLabel : `test${maxID + 1}`
+      });
     },
     nodeClick(id) {
-      console.log('node click', id);
+      window.console.log("node click", id);
     },
     nodeDelete(id) {
-      console.log('node delete', id);
+      window.console.log("node delete", id);
     },
     linkBreak(id) {
-      console.log('link break', id);
+      window.console.log("link break", id);
     },
     linkAdded(link) {
-      console.log('new link added:', link);
+      window.console.log("new link added:", link);
     }
   }
-}
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/FlowchartNode.vue
+++ src/components/FlowchartNode.vue
@@ -1,56 +1,54 @@
 <template>
-  <div class="flowchart-node" :style="nodeStyle" 
+  <div
+    class="flowchart-node"
+    :style="nodeStyle"
     @mousedown="handleMousedown"
     @mouseover="handleMouseOver"
     @mouseleave="handleMouseLeave"
-    v-bind:class="{selected: options.selected === id}">
-    <div class="node-port node-input"
-       @mousedown="inputMouseDown"
-       @mouseup="inputMouseUp">
-    </div>
+    v-bind:class="{selected: options.selected === id}"
+  >
+    <div class="node-port node-input" @mousedown="inputMouseDown" @mouseup="inputMouseUp"></div>
     <div class="node-main">
       <div v-text="type" class="node-type"></div>
       <div v-text="label" class="node-label"></div>
     </div>
-    <div class="node-port node-output" 
-      @mousedown="outputMouseDown">
-    </div>
+    <div class="node-port node-output" @mousedown="outputMouseDown"></div>
     <div v-show="show.delete" class="node-delete">&times;</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'FlowchartNode',
+  name: "FlowchartNode",
   props: {
     id: {
       type: Number,
       default: 1000,
       validator(val) {
-        return typeof val === 'number'
+        return typeof val === "number";
       }
     },
     x: {
       type: Number,
       default: 0,
       validator(val) {
-        return typeof val === 'number'
+        return typeof val === "number";
       }
-    },    
+    },
     y: {
       type: Number,
       default: 0,
       validator(val) {
-        return typeof val === 'number'
+        return typeof val === "number";
       }
     },
     type: {
       type: String,
-      default: 'Default'
+      default: "Default"
     },
     label: {
       type: String,
-      default: 'input name'
+      default: "input name"
     },
     options: {
       type: Object,
@@ -58,35 +56,37 @@ export default {
         return {
           centerX: 1024,
           scale: 1,
-          centerY: 140,
-        }
+          centerY: 140
+        };
       }
     }
   },
   data() {
     return {
       show: {
-        delete: false,
+        delete: false
       }
-    }
+    };
   },
-  mounted() {
-  },
+  mounted() {},
   computed: {
     nodeStyle() {
       return {
-        top: this.options.centerY + this.y * this.options.scale + 'px', // remove: this.options.offsetTop + 
-        left: this.options.centerX + this.x * this.options.scale + 'px', // remove: this.options.offsetLeft + 
-        transform: `scale(${this.options.scale})`,
-      }
+        top: this.options.centerY + this.y * this.options.scale + "px", // remove: this.options.offsetTop +
+        left: this.options.centerX + this.x * this.options.scale + "px", // remove: this.options.offsetLeft +
+        transform: `scale(${this.options.scale})`
+      };
     }
   },
   methods: {
     handleMousedown(e) {
       const target = e.target || e.srcElement;
       // console.log(target);
-      if (target.className.indexOf('node-input') < 0 && target.className.indexOf('node-output') < 0) {
-        this.$emit('nodeSelected', e);
+      if (
+        target.className.indexOf("node-input") < 0 &&
+        target.className.indexOf("node-output") < 0
+      ) {
+        this.$emit("nodeSelected", e);
       }
       e.preventDefault();
     },
@@ -97,18 +97,18 @@ export default {
       this.show.delete = false;
     },
     outputMouseDown(e) {
-      this.$emit('linkingStart')
+      this.$emit("linkingStart");
       e.preventDefault();
     },
     inputMouseDown(e) {
       e.preventDefault();
     },
     inputMouseUp(e) {
-      this.$emit('linkingStop')
+      this.$emit("linkingStop");
       e.preventDefault();
-    },
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -125,7 +125,7 @@ $portSize: 12;
   border: none;
   background: white;
   z-index: 1;
-  opacity: .9;
+  opacity: 0.9;
   cursor: move;
   transform-origin: top left;
   .node-main {
@@ -155,10 +155,10 @@ $portSize: 12;
     }
   }
   .node-input {
-    top: #{-2+$portSize/-2}px;
+    top: #{-2 + $portSize/-2}px;
   }
   .node-output {
-    bottom: #{-2+$portSize/-2}px;
+    bottom: #{-2 + $portSize/-2}px;
   }
   .node-delete {
     position: absolute;
@@ -173,7 +173,7 @@ $portSize: 12;
     border: 1px solid $themeColor;
     border-radius: 100px;
     text-align: center;
-    &:hover{
+    &:hover {
       background: $themeColor;
       color: white;
     }
